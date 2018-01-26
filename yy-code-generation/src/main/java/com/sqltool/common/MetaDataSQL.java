784f7fcd0f275c1554629b9fb601b99e06c95bc9
Tyler <36295238@qq.com>
2018-01-26 16:34:59 +0800

    初始化导入...

--- yy-code-generation/src/main/java/com/sqltool/common/MetaDataSQL.java
+++ yy-code-generation/src/main/java/com/sqltool/common/MetaDataSQL.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 
 /**
- * 
+ *
  * @author yangyu
  *
  */
@@ -18,7 +18,7 @@ public class MetaDataSQL {
 	 * @param metaData
 	 * @throws Exception
 	 */
-	public static String insertSQL(ResultSetMetaData metaData) throws Exception{
+	static String insertSQL(ResultSetMetaData metaData) throws Exception{
 		StringBuffer sb = new StringBuffer();
 		StringBuffer sbValues = new StringBuffer();
 		sb.append("INSERT INTO ");
@@ -32,20 +32,19 @@ public class MetaDataSQL {
 		String sql = sb.deleteCharAt(sb.length()-1).toString()+") VALUES (" + sbValues.deleteCharAt(sbValues.length()-1).toString() + ")";
 		return sql;
 	}
-	
+
 	/**
 	 * @param metaData
 	 * @throws Exception
 	 */
-	
-	public static String colBuilder(ResultSetMetaData metaData) throws Exception {
+
+	static String colBuilder(ResultSetMetaData metaData) throws Exception {
 
 		String newSObj = "\t\nStringBuffer colBuilder = new StringBuffer();\n";
 		String sbCol = "colBuilder.append(\" " + colStr(metaData) + "\");\n";
-		String sql = newSObj.concat(sbCol)
-				.concat("colBuilder.append(whereBuilder(example));\n")
-				.concat("return colBuilder.toString()");
-		return sql;
+		return newSObj.concat(sbCol)
+                .concat("colBuilder.append(whereBuilder(example));\n")
+                .concat("return colBuilder.toString()");
 	}
 
 	/**
@@ -54,17 +53,16 @@ public class MetaDataSQL {
 	 * @return
 	 * @throws Exception
 	 */
-	public static String colBuilderPage(ResultSetMetaData metaData) throws Exception {
+	static String colBuilderPage(ResultSetMetaData metaData) throws Exception {
 		String newSObj = "\t\nStringBuffer colBuilder = new StringBuffer();\n";
 		String sbCol = "colBuilder.append(\" " + colStr(metaData) + "\");\n";
-		String sql = newSObj.concat(sbCol)
-				.concat("colBuilder.append(whereBuilder(example));\n")
-				.concat("colBuilder.append(\" limit \");\n")
-				.concat("colBuilder.append((example.getSkip()-1) * example.getLimit());\n")
-				.concat("colBuilder.append(\",\");\n")
-				.concat("colBuilder.append(example.getLimit());\n")
-				.concat("return colBuilder.toString()");
-		return sql;
+		return newSObj.concat(sbCol)
+                .concat("colBuilder.append(whereBuilder(example));\n")
+                .concat("colBuilder.append(\" limit \");\n")
+                .concat("colBuilder.append((example.getSkip()-1) * example.getLimit());\n")
+                .concat("colBuilder.append(\",\");\n")
+                .concat("colBuilder.append(example.getLimit());\n")
+                .concat("return colBuilder.toString()");
 	}
 
 	/**
@@ -73,13 +71,12 @@ public class MetaDataSQL {
 	 * @return
 	 * @throws Exception
 	 */
-	public static String colBuilderPageCount(ResultSetMetaData metaData) throws Exception {
+	static String colBuilderPageCount(ResultSetMetaData metaData) throws Exception {
 		String newSObj = "\t\nStringBuffer colBuilder = new StringBuffer();\n";
 		String sbCol = "colBuilder.append(\" " + colStrCount(metaData) + "\");\n";
-		String sql = newSObj.concat(sbCol)
-				.concat("colBuilder.append(whereBuilder(example));\n")
-				.concat("return colBuilder.toString()");
-		return sql;
+		return newSObj.concat(sbCol)
+                .concat("colBuilder.append(whereBuilder(example));\n")
+                .concat("return colBuilder.toString()");
 	}
 
 	/**
@@ -88,7 +85,7 @@ public class MetaDataSQL {
 	 * @return
 	 * @throws Exception
 	 */
-	public static String colStr(ResultSetMetaData metaData) throws Exception {
+	private static String colStr(ResultSetMetaData metaData) throws Exception {
 		StringBuffer sb = new StringBuffer();
 		sb.append("SELECT ");
 		sb.append(fields(metaData));
@@ -104,15 +101,15 @@ public class MetaDataSQL {
 	 * @return
 	 * @throws Exception
 	 */
-	public static String colStrCount(ResultSetMetaData metaData) throws Exception {
+	private static String colStrCount(ResultSetMetaData metaData) throws Exception {
 		StringBuffer sb = new StringBuffer();
 		sb.append("SELECT COUNT(*) FROM ");
 		sb.append(metaData.getTableName(1));
 		sb.append(" WHERE 1=1 ");
 		return sb.toString();
 	}
-	
-	public static String selectById(ResultSetMetaData metaData) throws Exception {
+
+	static String selectById(ResultSetMetaData metaData) throws Exception {
 		StringBuffer sb = new StringBuffer();
 		sb.append("SELECT ");
 		sb.append(fields(metaData));
@@ -121,11 +118,11 @@ public class MetaDataSQL {
 		sb.append(" WHERE id = #{id}");
 		return sb.toString();
 	}
-	
+
 	/**
 	 * 字段信息
 	 */
-	public static String fields(ResultSetMetaData metaData) throws SQLException {
+	private static String fields(ResultSetMetaData metaData) throws SQLException {
 		StringBuffer sb = new StringBuffer();
 		for (int i = 1; i < metaData.getColumnCount() + 1; i++) {
 			String columnName = metaData.getColumnName(i);
@@ -134,43 +131,43 @@ public class MetaDataSQL {
 		sb.deleteCharAt(sb.length() - 1);
 		return sb.toString();
 	}
-	
+
 	/**
 	 * 动态拼接where条件
 	 * SQL where
 	 * @param metaData
 	 * @throws Exception
 	 */
-	
-	public static String whereBuilder(ResultSetMetaData metaData) throws Exception {
+
+	static String whereBuilder(ResultSetMetaData metaData) throws Exception {
 		StringBuffer sbWhere = new StringBuffer();
-		
+
 		String newSbObj = "\t\nStringBuffer whereBuilder = new StringBuffer();\n";
 		for (int i = 1; i < metaData.getColumnCount()+1; i++) {
 			String columnName = metaData.getColumnName(i);
 
-			sbWhere.append("\t\nif (example.get" + CommonUtils.captureName(CommonUtils.camelCaseName(columnName)) + "() != null){\n" 
+			sbWhere.append("\t\nif (example.get" + CommonUtils.captureName(CommonUtils.camelCaseName(columnName)) + "() != null){\n"
 			+ "\twhereBuilder.append(\" and " + columnName + "=#{"+ CommonUtils.camelCaseName(columnName) + "}\"); \n}\n");
 		}
 		return newSbObj.concat(sbWhere.toString()).concat("return whereBuilder.toString()");
 	}
-	
+
 	/**
 	 * 动态拼接更新字段
 	 * SQL update
 	 * @param metaData
 	 * @throws Exception
 	 */
-	
-	public static String updateBuilder(ResultSetMetaData metaData) throws Exception {
+
+	static String updateBuilder(ResultSetMetaData metaData) throws Exception {
 		StringBuffer sbWhere = new StringBuffer();
-		
+
 		String newSbObj = "\t\nStringBuffer updateBuilder = new StringBuffer();\n";
 		sbWhere.append("updateBuilder.append(\"UPDATE " + metaData.getTableName(1) + " SET \");");
 		for (int i = 1; i < metaData.getColumnCount()+1; i++) {
 			String columnName = metaData.getColumnName(i);
-			if("id".equals(columnName)) continue;
-			sbWhere.append("\t\nif (example.get" + CommonUtils.captureName(CommonUtils.camelCaseName(columnName)) + "() != null){\n" 
+			if("id".equals(columnName)) { continue; }
+			sbWhere.append("\t\nif (example.get" + CommonUtils.captureName(CommonUtils.camelCaseName(columnName)) + "() != null){\n"
 					+ "\tupdateBuilder.append(\"" + columnName + "=#{"+ CommonUtils.camelCaseName(columnName) + "}");
 			sbWhere.append(", ");
 			sbWhere.append("\"); \n}\n");
@@ -179,18 +176,18 @@ public class MetaDataSQL {
 		sbWhere.append("updateBuilder.append(\" WHERE id = #{id}\");\n");
 		return newSbObj.concat(sbWhere.toString()).concat("return updateBuilder.toString()");
 	}
-	
+
 	/**
 	 * @param metaData
 	 * @return
 	 * @throws Exception
 	 */
-	public static String deleteById(ResultSetMetaData metaData) throws Exception {
+	static String deleteById(ResultSetMetaData metaData) throws Exception {
 		StringBuffer sb = new StringBuffer();
 		sb.append("DELETE FROM ");
 		sb.append(metaData.getTableName(1));
 		sb.append(" WHERE id = #{id} ");
 		return sb.toString();
 	}
-	
+
 }
